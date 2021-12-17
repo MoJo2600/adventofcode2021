aa670ff596520cb64afff478f806d73a9f5580c2
Chrsitian Erhardt <christian.erhardt@mojo2k.de>
2021-12-17 14:55:32 +0000

    Added day3 part1

--- day3/index.py
+++ day3/index.py
@@ -7,41 +7,53 @@ for num in input:
         if num & (1 << 11-i):
             pos[i] = pos[i] +1
 
+print(','.join(str(x) for x in pos))
+
 gamma_rate = 0b0
 num_items = len(input)
-for i in range(0, len(pos)-1):
+for i in range(0, 11):
     if pos[i] > num_items/2:
-        gamma_rate |= (1 << len(pos)-1-i)
+        gamma_rate |= (1 << 11-i)
 
+# inverse gamma_rate to get epsilon
 epsilon_rate = gamma_rate ^ (2 ** 12 -1)
 print(f'{gamma_rate*epsilon_rate}')
 print(f'gamma_rate:       {bin(gamma_rate)}')
 print(f'epsilon_rate:     {bin(epsilon_rate)}')
 
+# b
+oxygen_generator_rating = 0b0
+co2_scrubber_rating = 0b0
 
-# string verify
-epsilon_rate_str = ''.join(['1' if pos[i] < num_items/2 else '0' for i in range(0, len(pos))]) 
-gamma_rate_str = ''.join(['1' if pos[i] > num_items/2 else '0' for i in range(0, len(pos))]) 
-print(f'gamma_rate_str:   {gamma_rate_str}')
-print(f'epsilon_rate_str: {epsilon_rate_str}')
+result_count = len(input)
+test = 0b0
+test2 = 0b0
+skip = 0b111111111111
+skip2 = 0b111111111111
+out = input
+out2 = input
+for i in range (0,12):
+    bit_value_at_position = 1 if gamma_rate & (1 << 12-i) else 0
+    test |= (bit_value_at_position << 12-i)
+    skip &= ~(1 << 12-i) | test
+    out = list(filter(lambda reading: reading >= test and reading <= skip, out))
+    if len(out) == 1:
+        print(f'found oxygen_generator_rating: {bin(out[0])} - {out[0]}')
+        oxygen_generator_rating = out[0]
 
-print(f'{int(gamma_rate_str, 2)*int(epsilon_rate_str, 2)}')
-
-# print(f'{0010100011000
-# 1101011100111')
-
-#for exponent in range (11,0):
-#    out = filter(lambda list_element: int(list_element, 2) < 2**(exponent+1) and int(list_element,2) >= 2**exponent, input)
-#    print(list(out))
+    bit_value_at_position = 1 if epsilon_rate & (1 << 12-i) else 0
 
 
 
 
-# out = filter(lambda val: val > 2**11, input)
-# print(list(out))
+    test2 |= (bit_value_at_position << 12-i)
+    skip2 &= ~(1 << 12-i) | test2
+    out2 = list(filter(lambda reading: reading <= test2 and reading >= skip2, out2))
+    if len(out2) == 1:
+        print(f'found co2_scrubber_rating: {bin(out2[0])} - {out2[0]}')
+        co2_scrubber_rating = out2[0]
 
-# out = filter(lambda val: val < 2**11 and val >= 2**10, input)
-# print(list(out))
+print(f'oxygen_generator_rating: {oxygen_generator_rating}')
+print(f'co2_scrubber_rating: {oxygen_generator_rating}')
 
-# out = filter(lambda val: val < 2**10 and val >= 2**9, input)
-# print(list(out))
+print(f'Result: {oxygen_generator_rating*oxygen_generator_rating}')
